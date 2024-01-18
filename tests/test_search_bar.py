5f8b352d573746c092043872679444f09b8a142f
Irina Kirillova <{ID}+{username}@users.noreply.github.com>
2024-01-18 22:54:58 +0600

    fix test cases

--- tests/test_search_bar.py
+++ tests/test_search_bar.py
@@ -2,7 +2,9 @@ import allure
 from allure_commons.types import Severity
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
+
 from intertop_tests.utils.country_selection import choice_of_country
+
 
 @allure.tag("mobile")
 @allure.severity(Severity.CRITICAL)
@@ -26,14 +28,12 @@ def test_search():
         browser.element((AppiumBy.ID, 'ua.mad.intertop:id/nextButton')).should(
             have.text('Далее')).click()
 
-
     with allure.step('Выбраем опцию "не включать уведомления"'):
         browser.element((AppiumBy.ID, 'ua.mad.intertop:id/notNowNotificationButton')).should(
             have.text('Не сейчас')).click()
 
     with allure.step('Переходим на страницу шоппинга'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Shopping')).click()
-
 
     with allure.step('Выполняем поиск товара по вкладке "Мужчинам"'):
         browser.element((AppiumBy.ID, 'ua.mad.intertop:id/categoriesSearchIcon')).click()
@@ -43,9 +43,7 @@ def test_search():
         browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Кроссовки"]')).click()
 
     with allure.step('Проверяем товары в выдаче'):
-        results = browser.all((AppiumBy.XPATH, '//android.widget.FrameLayout'))
+        results = browser.all((AppiumBy.XPATH, '//android.widget.TextView[@resource-id="ua.mad.intertop:id/productCartName"]'))
         results.should(have.size_greater_than(0))
-
-
-
+        results.first.should(have.text('Кроссовки'))
 
--- tests/test_search_from_catalog_girls.py
+++ tests/test_search_from_catalog_girls.py
@@ -11,7 +11,7 @@ from intertop_tests.utils.country_selection import choice_of_country
 @allure.feature("Поиск")
 @allure.story("Поиск из каталога")
 @allure.title("Поиск из каталога товара для девочек")
-def test_my_interests():
+def test_search_catalog():
     with allure.step('Выбираем страну '):
         choice_of_country()
 
@@ -36,12 +36,23 @@ def test_my_interests():
     with allure.step('Открываем каталог'):
         browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Каталог"]')).click()
 
-    with allure.step("Выбраем вкладку товаров для девочек "):
-        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Девочкам"]')).click()
-
     with allure.step("Выбираем пункт Одежда "):
         browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Одежда"]')).click()
 
     with allure.step('Проверяем что товары есть в выдаче'):
         results = browser.all((AppiumBy.XPATH, '//android.widget.FrameLayout'))
         results.should(have.size_greater_than(0))
+        product_1 = browser.element((AppiumBy.XPATH,
+                                  '//android.widget.FrameLayout[@resource-id="ua.mad.intertop:id/productCardViewListing"])[1]'))
+    with allure.step("Выбраем вкладку товаров для девочек "):
+        browser.element((AppiumBy.ID, 'ua.mad.intertop:id/genderGirls')).click()
+
+    with allure.step("Проверяем что выборка товаров сменилась "):
+        with allure.step('Проверяем товары в выдаче'):
+            results = browser.all((AppiumBy.XPATH, '//android.widget.FrameLayout'))
+            results.should(have.size_greater_than(0))
+            product_2 = browser.element((AppiumBy.XPATH,
+                                      '//android.widget.FrameLayout[@resource-id="ua.mad.intertop:id/productCardViewListing"])[1]'))
+
+
+            assert product_1 != product_2, ("Товары не могут быть одинаковыми")
